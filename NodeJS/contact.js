bef7e91b11f6555426d815d21765dc0392a5e001
Andrii Deleniv <67198751+deleniv88@users.noreply.github.com>
2023-01-07 08:27:05 +0100

    Update contact.js

--- NodeJS/contact.js
+++ NodeJS/contact.js
@@ -1,4 +1,3 @@
-// const fs = require("fs");
 const fs = require('fs/promises')
 const path = require("path");
 
@@ -6,37 +5,60 @@ const constactsPath = path.resolve(__dirname, "./db/contacts.json");
 
 const { nanoid } = require("nanoid");
 
-async function listContacts(){
-    const dbRaw = await fs.readFile(constactsPath, "utf-8");
-    const db = JSON.parse(dbRaw)
-    return db;
+async function listContacts() {
+    try {
+        const dbRaw = await fs.readFile(constactsPath, "utf-8");
+        const db = JSON.parse(dbRaw)
+        return db;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-async function writeDB(db){
-   await fs.writeFile(constactsPath, JSON.stringify(db, null, 4))
+async function writeDB(db) {
+    try {
+        await fs.writeFile(constactsPath, JSON.stringify(db, null, 4))
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-async function addContact(name, email, phone){
-    const id = nanoid();
-    const contact = {id, name, email, phone}
+async function addContact(name, email, phone) {
+    try {
+        const id = nanoid();
+        const contact = { id, name, email, phone }
 
-    const db = await listContacts();
-    db.push(contact)
+        const db = await listContacts();
+        db.push(contact)
 
-    await writeDB(db)
+        await writeDB(db)
+    } catch (error) {
+        console.error(error);
+    }
+
 }
 
-async function removeContact(contactId){
-    const db = await listContacts();
+async function removeContact(contactId) {
+    try {
+        const db = await listContacts();
 
-    const updateDB = db.filter((contact) => contact.id !== contactId)
-    await writeDB(updateDB)
+        const updateDB = db.filter((contact) => contact.id !== contactId)
+        await writeDB(updateDB)
+    } catch (error) {
+        console.error();
+    }
+
 }
 
-async function getContactById(contactId){
-    const db = await listContacts();
-    const searchById = db.find(({id}) => `${id}` === `${contactId}`);
-    return searchById;
+async function getContactById(contactId) {
+    try {
+        const db = await listContacts();
+        const searchById = db.find(({ id }) => `${id}` === `${contactId}`);
+        return searchById;
+    } catch (error) {
+        console.error(error);
+    }
+
 }
 
 module.exports = {
